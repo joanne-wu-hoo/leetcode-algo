305c57a2a7bea490a0565996ee3638c0d7c89229
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2022-07-31 10:11:36 -0700

    Create 167-two-sum-ii-input-array-is-sorted.js

---
+++ 167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+var twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length-1;
+    
+    while (left < right){
+        let sum = numbers[left] + numbers[right];
+        if (sum === target) return [left+1, right+1];
+        if (sum < target) left++;
+        if (sum > target) right--;
+    }
+}
