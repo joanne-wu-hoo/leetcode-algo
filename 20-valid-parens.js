d46ea35a9141cc5cfea5a96db5ac8b394f207f7e
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2024-08-23 09:02:13 -0700

    Update 20-valid-parens.js

--- 20-valid-parens.js
+++ 20-valid-parens.js
@@ -1,32 +1,37 @@
 // https://leetcode.com/problems/valid-parentheses/
 
-const parenPairs = {
-    "(": ")",
-    "[": "]",
-    "{": "}"
-}
-
 /*
-iterate thru the string and keep track of the parens we've seen with a stack
-at each value, check if the current paren closes the last paren in the stack
-- YES --> remove the last entry of the stack
-- NO --> add the cur paren to the stack
+Whenever we encounter an open paren, add to the stack
+Whenever we encounter a close paren, pop last item off of stack 
+- if the parens do NOT match, return false
+- otherwise, keep going
 
 at the end if the stack is empty, return true; otherwise, return false
 */
 
+var PAREN_PAIRS = {
+    "{": "}",
+    "(": ")",
+    "[": "]"
+}
+
+var OPEN_PARENS = Object.keys(PAREN_PAIRS)
+var CLOSE_PARENS = Object.values(PAREN_PAIRS)
+
 var isValid = function(s) {
-    let seenParens = [];
+    const seenParens = []
     
-    for (let paren of s){
-        const lastSeenParen = seenParens[seenParens.length-1];
-        if (parenPairs[lastSeenParen] === paren) {
-            seenParens.pop();
+    for (let paren of s) {
+        if (OPEN_PARENS.includes(paren)) {
+            seenParens.push(paren)
         } else {
-            seenParens.push(paren);
+            const lastSeenParen = seenParens.pop()
+            if (PAREN_PAIRS[lastSeenParen] !== paren) return false
         }
     }
-    
-  
-    return seenParens.length === 0;
-}
+
+    // if there are any unmatched parens at the end, invalid string
+    return seenParens.length === 0
+};
+
+
