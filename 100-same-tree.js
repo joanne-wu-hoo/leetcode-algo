7b2ae7cf86faf04e7be1b52b5f224cd095265fa4
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2024-09-05 19:05:43 -0700

    Update 100-same-tree.js

--- 100-same-tree.js
+++ 100-same-tree.js
@@ -1,8 +1,38 @@
 // https://leetcode.com/problems/same-tree/
 
+/*
+NOT the same tree if
+p.left is null but q.left exists (and vice versa)
+p.right is null but q.right exists (and vice versa)
+
+same tree if:
+p.left = q.left and p.right = q.right 
+
+*/
+
 var isSameTree = (p,q) => {
     if (!p && !q) return true;
     if (!p || !q) return false;
     if (p.val !== q.val) return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }
+
+
+var isSameTree = function(p, q) {
+    if (!p && !q) return true
+    if (!p && q) return false
+    if (p && !q) return false
+
+    let stack = [[p,q]]
+
+    while (stack.length) {
+        const [left, right] = stack.pop()
+        if (!left && !right) continue
+        if (!left && right) return false
+        if (left && !right) return false
+        if (left.val !== right.val) return false
+        stack.push([left.left, right.left], [left.right, right.right])
+    }
+
+    return true
+};
